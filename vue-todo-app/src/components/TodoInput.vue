43fffdeb6b8e3fef276760c88fc71f00f372df66
LeeSeHee <euc728@naver.com>
2021-08-10 17:30:09 +0900

    inputBox localStorage 저장 로직

--- vue-todo-app/src/components/TodoInput.vue
+++ vue-todo-app/src/components/TodoInput.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-      input
+      <input type="text" v-model="newTodoItem">
+      <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
+    data: function(){
+        return{
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function(){
+            //저장하는 로직
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = ''; //초기화
+        },
 
+    }
 }
 </script>
 
